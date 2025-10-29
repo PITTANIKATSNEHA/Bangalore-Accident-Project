98b369ec6b044b1867e74419c5f5fa69e70b1188
Sneha Pittanikat <pittanikatsneha@gmail.com>
2025-10-29 11:56:42 +0530

    Update create_tables.sql

--- Snowflake/create_tables.sql
+++ Snowflake/create_tables.sql
@@ -1,3 +1,15 @@
+-- Create a new database for your project
+CREATE DATABASE BTP_ANALYTICS;
+
+-- Use the database
+USE DATABASE BTP_ANALYTICS;
+
+-- Create schema for raw data
+CREATE SCHEMA RAW_DATA;
+
+-- Create schema for analytics
+CREATE SCHEMA ANALYTICS;
+-- create tables 
 CREATE TABLE RAW_DATA.city_crashes (
     year INT, fatal_crashes INT, killed INT, non_fatal_crashes INT, total_crashes INT, injured INT
 );
